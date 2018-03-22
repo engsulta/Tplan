7f1207bdad5b5022a59ad8df68f36d2baf9f06dd
engsulta <en.ahmedsultan@gmail.com>
2018-03-22 08:31:34 -0700

    required in interfaces  changes

--- app/src/main/java/com/example/sulta/tplan/view/activities/interfaces/IHomeActivity.java
+++ app/src/main/java/com/example/sulta/tplan/view/activities/interfaces/IHomeActivity.java
@@ -1,14 +1,10 @@
 package com.example.sulta.tplan.view.activities.interfaces;
 
-import com.example.sulta.tplan.model.Trip;
-
-import java.util.ArrayList;
-
 /**
  * Created by Passant on 3/17/2018.
  */
 
 public interface IHomeActivity {
-    ArrayList<Trip>getMyUpcomingTrips();
-    void loadMyUpcomingTrips();
+   // ArrayList<Trip>getMyUpcomingTrips();
+    //void loadMyUpcomingTrips();
 }
