f6e83cf36d7fb764ca79951a4cf37f12a012df07
PassantAhmed <passantswelum@outlook.com>
2018-03-22 23:45:57 +0200

    changes

---
+++ app/src/main/java/com/example/sulta/tplan/presenter/adapters/HomePagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.sulta.tplan.presenter.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.sulta.tplan.view.fragments.HistoryTrips;
+import com.example.sulta.tplan.view.fragments.Settings;
+import com.example.sulta.tplan.view.fragments.UpComingTrips;
+
+/**
+ * Created by Passant on 3/21/2018.
+ */
+
+public class HomePagerAdapter extends FragmentStatePagerAdapter {
+    int num_taps;
+    public HomePagerAdapter(FragmentManager fm, int num_taps) {
+        super(fm);
+        this.num_taps=num_taps;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                UpComingTrips tab1 = new UpComingTrips();
+                return tab1;
+            case 1:
+                HistoryTrips tab2 = new HistoryTrips();
+                return tab2;
+            case 2:
+                Settings tab3 = new Settings();
+                return tab3;
+            default:
+                return null;  }}
+
+    @Override
+    public int getCount() {
+        return num_taps;
+    }
+}
