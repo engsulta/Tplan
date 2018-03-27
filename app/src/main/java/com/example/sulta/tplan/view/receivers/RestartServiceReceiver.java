9ef3187ea1e2d9b09a9fbfcd576a1ce749c54fef
engsulta <en.ahmedsultan@gmail.com>
2018-03-27 09:21:02 -0700

    before pull new home

--- app/src/main/java/com/example/sulta/tplan/view/receivers/RestartServiceReceiver.java
+++ app/src/main/java/com/example/sulta/tplan/view/receivers/RestartServiceReceiver.java
@@ -8,16 +8,22 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 
 import com.example.sulta.tplan.database.SqlAdapter;
+import com.example.sulta.tplan.model.Trip;
 import com.example.sulta.tplan.view.services.ReminderService;
 
+import java.util.ArrayList;
+
 public class RestartServiceReceiver extends BroadcastReceiver {
-    ReminderService myService;
-    boolean isBound=false;
-    SqlAdapter db;
+    private ReminderService myService;
+    private boolean isBound=false;
+    private SqlAdapter db;
+    private Context context;
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED))
         {
+
+            this.context=context;
              db=new SqlAdapter(context);
             Intent myintent=new Intent(context,ReminderService.class);
             context.bindService(intent,myconnection, Context.BIND_AUTO_CREATE);
@@ -49,15 +55,19 @@ public class RestartServiceReceiver extends BroadcastReceiver {
         }
     };
     private void restartAlarmSetting() {
-        // ArrayList<Trip> trips=db.getalltrips();
-        //for (Trip x:trips){
+         ArrayList<Trip> trips=db.selectAllTrips();
+        for (Trip x:trips) {
+            if(System.currentTimeMillis()<x.startTimeMillis) {
+                myService.startNewAlarm(this.context, (long) x.getstartTimeMillis(), x.getId());//send request conde from trip id
+            }
+            else {
+                //edit trip to make status past
+                x.setStatus("missed");
 
+                //
+            }
 
-        //   new Alarm().setAlarm(context,Long.parseLong(x.getAlarm()) ,x.getId());
-       // myService.startNewAlarm(this,x.getTimeInMillis(),10);//send request conde from trip id
-
-        // }
-
+        }
 
     }
 
