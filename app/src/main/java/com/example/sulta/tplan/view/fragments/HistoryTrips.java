776e621e2e9cafd7b72756f8979a56bfffb50847
engsulta <en.ahmedsultan@gmail.com>
2018-03-29 08:04:33 -0700

    DoneTripsMap

--- app/src/main/java/com/example/sulta/tplan/view/fragments/HistoryTrips.java
+++ app/src/main/java/com/example/sulta/tplan/view/fragments/HistoryTrips.java
@@ -1,6 +1,7 @@
 package com.example.sulta.tplan.view.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,6 +17,7 @@ import com.example.sulta.tplan.database.SqlAdapter;
 import com.example.sulta.tplan.model.Trip;
 import com.example.sulta.tplan.presenter.HomeActivityPresenter;
 import com.example.sulta.tplan.presenter.interfaces.IHomeActivityPresenter;
+import com.example.sulta.tplan.view.activities.DoneTripsMap;
 
 import java.util.ArrayList;
 
@@ -48,7 +50,9 @@ public class HistoryTrips extends Fragment {
 //                Intent intent = new Intent(getContext(), CreateTripActivity.class);
 //                getContext().startActivity(intent);
                 //sulta
-
+                Intent intent=new Intent(getContext(), DoneTripsMap.class);
+                //intent.putExtra("doneTrips",doneTrips);
+                startActivity(intent);
                 //sulta
             }
         });
@@ -65,12 +69,7 @@ public class HistoryTrips extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        db = new SqlAdapter(getContext());
-        if (savedInstanceState.get("doneTrips") != null) {
-            doneTrips = (ArrayList<Trip>) savedInstanceState.get("doneTrips");
-        } else {
-            doneTrips = db.selectDoneTrips();
-        }
+
     }
 
     @Override
@@ -87,7 +86,7 @@ public class HistoryTrips extends Fragment {
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        outState.putSerializable("doneTrips", doneTrips);
+//        outState.putSerializable("doneTrips", doneTrips);
 
     }
 }
